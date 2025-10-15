8063104c81302e4d9d10edc19422ffd4ae9cc6f0
dgpark0617 <44637300+dgpark0617@users.noreply.github.com>
2025-10-16 08:54:03 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -12,22 +12,25 @@
 [ point  ] we can't see the real, whole thing. and misunderstanding. 
 [ point  ] that make seperation from God. 
 
-[ point  ] by good things and by bad things (빌 1.15~18)
-[ point  ] the song. where is the love?  the disappointment. that you feel. 
-[ point  ] the disappoint that you feel now. God plan.
+[ part 1 ] limit ability of us
 
 [ time   ] length(GPS. satellite), difference in time. because we think the time is same.     // GPS 위성은 고도 약 20,200km상공을 돌면서 정확한 시간 정보를 지상의 수신기로 전송하려하지만 특수상대성이론에 의해 조금씩 시간이 다르게 흐르게 되는데요. GPS 위성은 시속 14,000km정도로 지구 주위를 공전하는데 빠르게 움직이며 지상보다 위성의 시간이 느리게 됩니다. 이에 따라 하루에 약 7마이크로초 차이가 발생하게 되며 일반상대성 이론에 의해 중력이 약한 곳에서 시간은 빠르게 가기에 GPS 위성은 지구 중력장에서 멀리 떨어져 있어 지상보다 중력의 영향이 작기에 약 45마이크로초 정도 시간이 빠르게 흐르게 됩니다.이에 따라 이를 보정하지 않으면 하루에 약 38마이크로초 빠르게 되는데, 시간오차를 바탕으로 이를 계산하면 광속이 30만km/s이기에 오차가 하루에 누적되면 10km이상이 되버리게 되는것이지요. 이에 따라 GPS 위성은 시계를 미리 느리게 조정해서 발사하게 되고 지상에서 지속적으로 위성 시간의 보정을 하게 되어 수신기에 여러 위성들이 오는 신호를 교차해 최소화된 오차를 도출하게 합니다.
 [ time   ] home to work. navigation route. more than 3 hours. the route I found. 2 hour 30. because we believe what we see.    
 
-[ place  ] mokpo. mountain. disappear (마. 17.20). because we beleive what we touch.  
-[ place  ] lee young do. golem. / the wall in front of you are exist just in your mind. 
-[ place  ] seohyeon. want to touch blue sky during driving. but she actually live in it already. 
+[ place  ] misunderstanding about eternitiy of object. mokpo. mountain. disappear (마. 17.20). because we beleive what we touch.  
+[ place  ] misunderstanding about location seperation. lee young do. golem. / the wall in front of you are exist just in your mind. 
+[ place  ] misunderstanding about the real place we lived in. seohyeon. want to touch blue sky during driving. but she actually live in it already. 
 
 [ place  ] gurea. hwaumsa. zirisan. mountain in the cloud. jangkwan. kanghwado. during the rain, mountain in the cloud. 
 [ place  ] when we winning about rain, the sight that I was looking for is already there. 
-[ place  ] we can see the rainbow, after the rain. 
+[ point  ] we can see the rainbow, after the rain. 
 
-[ people ] onesimus(빌 1.11), second son(누 15.20). peter(마 26.33) 
+[ people ] onesimus(빌 1.11). once bad people. but changed after peter. 
+[ people ] second son(누 15.20). once betray his father. but, come back. 
+[ people ] peter(마 26.33). once betray Jesus. but, come back.  
+
+
+[ part 2 ] my realization about my limitation
 
 [ sound  ] piano in the subway. (창 9.13)
 [ sound  ] just a normal sound, could be blessing for somebody else. 
@@ -50,3 +53,9 @@
 [ final  ] don't know the every sound is music for somebody else. 
 [ final  ] don't know that we already lived in the beautiful sky. 
 [ final  ] that make seperation. be together. 
+
+
+[ point  ] lee un joo. br. morning QT. 10/10
+[ point  ] by good things and by bad things (빌 1.15~18)
+[ point  ] the song. where is the love?  the disappointment. that you feel. 
+[ point  ] the disappoint that you feel now. God plan.
